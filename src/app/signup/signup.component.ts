7101a26a0adffbde197100ac543e2750e468da65
GoogleKhan <shadab105662@gmail.com>
2022-05-20 12:40:16 +0530

    First

---
+++ src/app/signup/signup.component.ts
@@ -0,0 +1,50 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from "@angular/forms";
+import { Router } from "@angular/router"
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+  public signupForm !: FormGroup;
+
+  registeredUsers: any = [];
+
+  constructor(private formBuilder: FormBuilder, private http: HttpClient, private router: Router) { }
+
+  ngOnInit(): void {
+    this.signupForm = this.formBuilder.group({
+      fullname: [''],
+      email: [''],
+      password: [''],
+      mobile: ['']
+    })
+  }
+  signUp() {
+
+    const obj = this.signupForm?.value;
+
+
+
+    this.registeredUsers.push(obj);
+    console.log(this.registeredUsers);
+
+    const jsonStr = JSON.stringify(this.registeredUsers);
+    console.log(jsonStr);
+
+    localStorage.setItem("RegisteredUsers", jsonStr)
+
+    // this.http.post<any>("http://localhost:3000/signupUsers", this.signupForm?.value)
+    //   .subscribe(res => {
+    //     alert("Sign Up Is Done Succesfully");
+    //     this.signupForm?.reset();
+    //     this.router.navigate(['login']);
+    //   },
+    //     err => {
+    //       alert("Something went wrong")
+
+    //     })
+  }
+}
